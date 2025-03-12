05907792fe12d5aff7c33661ab95abea0e202fc1
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-03-12 09:48:26 +0530

    same

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
       <div className='bg-black py-[23px] px-4'>
           <div className="flex justify-between max-w-[1140px] mx-auto gap-10">
-              <p className='font-normal md:text-sm text-xs leading-[150%] text-[#8D8D8D]'>Made with <span className='!text-[#8D8D8D]'>❤️</span> for the people of the internet.</p>
+              <p className='font-normal md:text-sm text-xs leading-[150%] text-[#8D8D8D]'>Made with ❤️ for the people of the internet.</p>
         <p className='font-normal md:text-sm text-xs text-[#8D8D8D] '>
                 © {currentYear} Dataskate.io, All Rights Reserved
               </p>
